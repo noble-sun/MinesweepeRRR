6b985f07cdb91f062f539e1d7aa4a443192bdf61
noble-sun <matsubayashi.helio+github@gmail.com>
2025-04-28 16:59:14 -0300

    fix: npm bug on ci

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+process.env.ROLLUP_USE_NODE_BUILTIN = 'true';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
