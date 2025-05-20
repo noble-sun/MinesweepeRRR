cde8e1f055efb29567e9f13a641524f69a92fba2
noble-sun <matsubayashi.helio+github@gmail.com>
2025-05-20 19:20:26 -0300

    test: place mines on board service

---
+++ backend/spec/services/place_mines_service_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe PlaceMinesService, type: :service do
+  describe ".call" do
+    context "randomly place a given number of mines in the minefield" do
+      it 'successfully' do
+        minefield = Minefield.new(size: 4)
+
+        expect(described_class.call(minefield:, mine_count: 5)).to be_truthy
+        expect(minefield.grid.flatten.filter { |cell| cell.mine? }.count).to eq(5)
+      end
+
+      context "when called twice or more for the same Minefield instance" do
+        context "overwrite the current mines and place new ones" do
+          it 'sucessfully' do
+            minefield = Minefield.new(size: 4)
+
+            described_class.call(minefield:, mine_count: 5)
+            described_class.call(minefield:, mine_count: 5)
+
+            expect(minefield.grid.flatten.filter { |cell| cell.mine? }.count).to eq(5)
+          end
+        end
+      end
+    end
+  end
+end
