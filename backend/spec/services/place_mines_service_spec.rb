f659e97ed943edb3b44c7db4f006514791f5bce0
noble-sun <matsubayashi.helio+github@gmail.com>
2025-05-20 21:39:59 -0300

    test: refactor test to use minefield method #mine_count

--- backend/spec/services/place_mines_service_spec.rb
+++ backend/spec/services/place_mines_service_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe PlaceMinesService, type: :service do
         minefield = Minefield.new(size: 4)
 
         expect(described_class.call(minefield:, mine_count: 5)).to be_truthy
-        expect(minefield.grid.flatten.filter { |cell| cell.mine? }.count).to eq(5)
+        expect(minefield.mine_count).to eq(5)
       end
 
       context "when called twice or more for the same Minefield instance" do
@@ -18,7 +18,7 @@ RSpec.describe PlaceMinesService, type: :service do
             described_class.call(minefield:, mine_count: 5)
             described_class.call(minefield:, mine_count: 5)
 
-            expect(minefield.grid.flatten.filter { |cell| cell.mine? }.count).to eq(5)
+            expect(minefield.mine_count).to eq(5)
           end
         end
       end
