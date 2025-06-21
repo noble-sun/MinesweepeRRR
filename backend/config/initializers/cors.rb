be5d4ef487d763d18335fc068249267b2ac20993
noble-sun <matsubayashi.helio+github@gmail.com>
2025-06-21 00:42:57 -0300

    style: ajust rubocop offenses

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,10 +8,10 @@ if Rails.env.development?
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins "http://localhost:5173"
-  
+
       resource "*",
         headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+        methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
     end
   end
 end
