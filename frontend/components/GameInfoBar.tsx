01b86bf3dee760a7f5c50d036a22439c34d60ffc
noble-sun <matsubayashi.helio+github@gmail.com>
2025-06-20 21:54:26 -0300

    style: three digit timer

--- frontend/components/GameInfoBar.tsx
+++ frontend/components/GameInfoBar.tsx
@@ -6,14 +6,15 @@ export default function GameInfoBar(
   { time, exploded, mineCount }:
   { time: number, exploded: boolean, mineCount: number }
 ) {
+  const paddedTime = time.toString().padStart(3, "0")
   return (
     <>
-      <div className="flex justify-between items-center grow">
-        <span> Mines: {mineCount}</span>
-        <div className="cursor-pointer" onClick={() => { window.location.reload() }}>
+      <div className="flex justify-between items-center py-3">
+        <span className="flex flex-1 justify-center border p-2"> Mines: {mineCount}</span>
+        <div className="cursor-pointer flex flex-1 justify-center" onClick={() => { window.location.reload() }}>
           { exploded ? <img src={deadFaceIcon}/> : <img src={smileFaceIcon} /> }
         </div>
-        <span> Time: {time} </span>
+        <span className="flex flex-1 justify-center border p-2"> Time: {paddedTime} </span>
       </div>
     </>
   )
