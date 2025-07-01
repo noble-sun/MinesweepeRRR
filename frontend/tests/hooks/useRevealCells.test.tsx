392f58392e0a06615c0ee4ca39585e1827580104
noble-sun <matsubayashi.helio+github@gmail.com>
2025-07-01 19:47:37 -0300

    test: add more tests for useRevealCells hook

--- frontend/tests/hooks/useRevealCells.test.tsx
+++ frontend/tests/hooks/useRevealCells.test.tsx
@@ -2,6 +2,7 @@ import { renderHook, waitFor, act } from '@testing-library/react'
 import { useRevealCells } from '../../hooks/useRevealCells'
 import { useGameContext } from '../../contexts/GameContext'
 import { renderHookWithGameContext } from '../test-utils'
+import * as expandCellsHelper from '../../helpers/adjacentCellsToExpand.ts'
 
 describe('useRevealCells', () => {
   describe('revealCell', () => {
@@ -54,6 +55,83 @@ describe('useRevealCells', () => {
 
         expect(setRevealedCellsSpy).not.toHaveBeenCalled()
         expect(setSafeUnrevealedCellsSpy).not.toHaveBeenCalled()
+      })
+    })
+  })
+
+  describe('expandAdjacentCells', () => {
+    describe('when cell clue value is zero', () => {
+      describe('when all adjacent cells have clue values above zero', () => {
+        it('add all adjacent cell keys to revealedCells state', () => {
+          const minefield = [
+            [
+              { x: 0, y: 0, mine: false, clue: 1},
+              { x: 0, y: 1, mine: false, clue: 1},
+              { x: 0, y: 2, mine: false, clue: 1},
+            ],
+            [
+              { x: 1, y: 0, mine: false, clue: 1},
+              { x: 1, y: 1, mine: false, clue: 0},
+              { x: 1, y: 2, mine: false, clue: 1},
+            ],
+            [
+              { x: 2, y: 0, mine: false, clue: 1},
+              { x: 2, y: 1, mine: false, clue: 1},
+              { x: 2, y: 2, mine: false, clue: 1},
+            ]
+          ]
+
+          const result = renderHookWithGameContext(() => ({
+            useRevealCellsResult: useRevealCells(minefield),
+            useGameContextResult: useGameContext()
+          }))
+
+          act(() => {
+            result.current!.useRevealCellsResult.expandAdjacentCells(1, 1)
+          })
+
+          const expectedCellKeys = ['0-0', '0-1', '0-2', '1-0', '1-2', '2-0', '2-1', '2-2'] 
+          expect([...result.current!.useGameContextResult.revealedCells]).toEqual(
+            expectedCellKeys
+          )
+        })
+      })
+
+      describe('when one of the adjacent cells clue value is zero', () => {
+        it('recursively add adjacent cell keys to revealedCells state to every'/
+           'revealed cell with clue value equal to zero', () => {
+          const minefield = [
+            [
+              { x: 0, y: 0, mine: false, clue: 0},
+              { x: 0, y: 1, mine: false, clue: 1},
+              { x: 0, y: 2, mine: false, clue: 1},
+            ],
+            [
+              { x: 1, y: 0, mine: false, clue: 0},
+              { x: 1, y: 1, mine: false, clue: 1},
+              { x: 1, y: 2, mine: true, clue: null},
+            ],
+            [
+              { x: 2, y: 0, mine: false, clue: 0},
+              { x: 2, y: 1, mine: false, clue: 1},
+              { x: 2, y: 2, mine: false, clue: 1},
+            ]
+          ]
+
+          const result = renderHookWithGameContext(() => ({
+            useRevealCellsResult: useRevealCells(minefield),
+            useGameContextResult: useGameContext()
+          }))
+
+          act(() => {
+            result.current!.useRevealCellsResult.expandAdjacentCells(0, 0)
+          })
+
+          const expectedCellKeys = ['0-1', '1-0', '1-1', '0-0', '2-0', '2-1']
+          expect([...result.current!.useGameContextResult.revealedCells]).toEqual(
+            expectedCellKeys
+          )
+        })
       })
     })
   })
