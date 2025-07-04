e8d59c302ebdca2cc024feba31cf90c91c56dfea
noble-sun <matsubayashi.helio+github@gmail.com>
2025-07-04 14:19:47 -0300

    refactor: move game states to hook useGameState

---
+++ frontend/hooks/useGameState.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+export function useGameState() {
+  const [mineCount, setMineCount] = useState(40)
+  const [gameWon, setGameWon] = useState<boolean | null>(null)
+
+  const updateMineCount = (value: number) => {
+    setMineCount((prevCount) => prevCount + value)
+  }
+
+  const updateGameStatus = (value: boolean) => {
+    setGameWon(value)
+  }
+
+  const gameStatusText = () => {
+    if (gameWon === null) return
+    const text = gameWon ? 'You Won!!!' : 'You Lost!'
+
+    return text
+  }
+
+  return {
+    mineCount,
+    gameWon,
+    updateMineCount,
+    updateGameStatus,
+    gameStatusText
+  }
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,23 @@
 import './App.css'
 
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 
 import GameInfoBar from '../components/GameInfoBar.tsx'
 import Minefield from '../components/Minefield.tsx'
 import { GameProvider } from '../contexts/GameContext.tsx'
+import { useGameState } from '../hooks/useGameState.tsx'
 import { useTimer } from '../hooks/useTimer.tsx'
 
 function App() {
   const { time, gameIsRunning, startTimer, stopTimer } = useTimer()
+  const { mineCount, updateMineCount, updateGameStatus, gameStatusText } = useGameState()
 
   const [exploded, setExploded] = useState(false)
 
-  const [mineCount, setMineCount] = useState(40)
-  const updateMineCount = (value: number) => {
-    setMineCount((prevCount) => prevCount + value)
-  }
-
-  const [gameWon, setGameWon] = useState<boolean | null>(null)
-  const updateGameStatus = (value: boolean) => {
-    setGameWon(value)
-  }
-
-  const renderGameStatus = () => {
-    if (gameWon === null) return
-    const text = gameWon ? 'You Won!!!' : 'You Lost!'
-
-    return <span>{text}</span>
-  }
   return (
     <>
       <GameProvider>
-        <div>{renderGameStatus()}</div>
+        <div>{gameStatusText()}</div>
         <GameInfoBar time={time} exploded={exploded} mineCount={mineCount} />
         <Minefield
           startTimer={startTimer}
